7ccbba8debdce55bcd7b975b6ea75ec58a4ca836
Erik Galler <43548211+erikgal@users.noreply.github.com>
2022-04-28 18:17:48 +0200

    a

--- myproject/settings.py
+++ myproject/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
