3f0c78b20085471d6668184afe8e6ea2c9e3c3dd
nilsmh <nmhillesund99@gmail.com>
2022-04-28 11:37:42 +0200

    removed /api endpoint in prod in config

--- src/constants/config.tsx
+++ src/constants/config.tsx
@@ -1,8 +1,8 @@
 const prod = {
     url: {
-        API_URL_USER: 'https://gib-2-project.herokuapp.com/api/user',
-        API_URL_PATH_GEOM: 'https://gib-2-project.herokuapp.com/api/path_geom',
-        API_URL_INTERSECT: 'https://gib-2-project.herokuapp.com/api/intersect',
+        API_URL_USER: 'https://gib-2-project.herokuapp.com/user',
+        API_URL_PATH_GEOM: 'https://gib-2-project.herokuapp.com/path_geom',
+        API_URL_INTERSECT: 'https://gib-2-project.herokuapp.com/intersect',
         SOCIAL_AUTH_FACEBOOK_KEY: '4554233111370018',
         SOCIAL_AUTH_FACEBOOK_SECRET: '3d6191526537b18b63a6cb3109915c6a',
         CLIENT_ID: 'cKyuK5Y36dtA2Aku8sSFkZuKUf1BQUMNTIpQ4hrR',
