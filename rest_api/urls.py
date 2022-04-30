ff337c052973066da65d07d5244981b12334f0d2
Erik Galler <43548211+erikgal@users.noreply.github.com>
2022-04-30 14:05:49 +0200

    a

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('user', views.UserViewset.as_view({'get': 'list'})),
     path('path_geom', views.Path_geomViewset.as_view({'post': 'create', 'get': 'list'})),
     path('intersect', views.Intersected_pathsViewset.as_view({'get': 'list'})),
-    re_path('.*',TemplateView.as_view(template_name='index.html')),    
+    #re_path('.*',TemplateView.as_view(template_name='index.html')),    
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
